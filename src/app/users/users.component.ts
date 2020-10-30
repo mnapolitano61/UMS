caf2f10a3eaf06f2455f31a5bd24ea33ad5cc20f
Maddalena Napolitano <maddalenapolitano@gmail.com>
2020-10-30 08:38:40 +0100

    create users component

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+}
