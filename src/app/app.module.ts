099ab9084fba0af305a4f62c2a7b8f4d88c49e6d
Maddalena Napolitano <maddalenapolitano@gmail.com>
2020-10-31 17:34:40 +0100

    ngOnInit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {UsersComponent} from './users/users.component';
+import {UserService} from './users/user.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import {UsersComponent} from './users/users.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
